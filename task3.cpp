f80c580d770ec20ae60f719c7ca2f9862cd548c8
jawwad <jawwad@ubuntu.myguest.virtualbox.org>
2025-11-12 23:14:28 +0500

    Part 1,2 and 3 of task3

---
+++ task3.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Account
+{
+	private:
+		int accountNumber, balance;
+	public:
+		int deposit(int a)
+		{
+			balance = balance+a;
+			cout<<"New Balance: "<<balance<<endl;
+			return balance;
+		}
+		int withdraw(int w)
+		{
+			balance = balance-w;
+			cout<<"Current balance: "<<balance<<endl;
+			return balance;
+		}
+		int displayBalance(int accNo)
+		{
+			cout<<"Acc no, :"<<accNo<<endl;
+			cout << "Account No:"<<accountNumber<<endl;
+			cout<<"Current Balance: "<<balance<<endl;
+			return balance;
+		}
+
+};
+
+
+int main()
+{
+	//deposit d;
+	//withdraw w;
+	//displayBalance b;
+	Account a;
+	int i,j;
+	cout<<"Class and Encapsulation Completed"<<endl;
+	cout<<"Do you want to 1) Deposit, 2) Withdraw, 3) Check Balance"<<endl;
+	cin >> i;
+	switch (i) 
+	{
+		case 1:
+			cout<<"Enter Amount: "<<endl;
+			cin>> j;
+			a.deposit(j);
+			break;
+		case 2:
+			cout<<"Enter Amount: "<<endl;
+			cin>>j;
+			a.withdraw(j);
+			break;
+		case 3:
+			cout<<"Enter account no. : "<<endl;
+			cin>>j;
+			a.displayBalance(j);
+			break;
+		default:
+			cout<<"Enter correct option no.!"<<endl;
+			break;
+	}
+
+	return 0;
+}
+
+			
+
