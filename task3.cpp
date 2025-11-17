5b0284c9232c06e77df793a471fb9ccfdad6d4cc
jawwad <jawwad@ubuntu.myguest.virtualbox.org>
2025-11-17 21:03:58 +0500

    TASK 3 COMPLETION

--- task3.cpp
+++ task3.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 class Account
 {
-	private:
-		int accountNumber, balance;
+	protected:
+		int accountNumber = 0, balance = 0;
+
 	public:
 		int deposit(int a)
 		{
@@ -12,11 +13,11 @@ class Account
 			cout<<"New Balance: "<<balance<<endl;
 			return balance;
 		}
-		int withdraw(int w)
+		virtual void  withdraw(int w)
 		{
 			balance = balance-w;
 			cout<<"Current balance: "<<balance<<endl;
-			return balance;
+			//return balance;
 		}
 		int displayBalance(int accNo)
 		{
@@ -25,18 +26,47 @@ class Account
 			cout<<"Current Balance: "<<balance<<endl;
 			return balance;
 		}
+		
+		int getBalance()
+	       	{
+		       	return balance; 
+		}
+   		 void setBalance(int b)
+		 {
+			 balance = b;
+		 }
 
 };
+
 
 class SavingsAccount : public Account {
 	public:
-		int withdraw (int amnt)
+		void withdraw (int amnt) override
 		{
 			if(amnt>50000)
 				cout<<"Withdraw Limit Exceed"<<endl;
-			return 0;
+			else 
+			{
+				cout << "Amount withdrawn: "<<endl;
+				Account::withdraw(amnt);
+			}
+				
 		}
 };
+
+class CurrentAccount : public Account {
+	public:
+		void withdraw (int amnt)
+		{
+			int charges = 10;	//10% charge
+			int deduct;
+			int newBal = getBalance();
+			deduct = (amnt*charges)/100;
+			cout <<"Amount witdrwn after charges: "<<amnt-deduct<<endl;
+			setBalance(getBalance() - (amnt - deduct));
+		}
+};
+
 
 
 int main()
@@ -45,6 +75,8 @@ int main()
 	//withdraw w;
 	//displayBalance b;
 	Account a;
+	SavingsAccount  s;
+	CurrentAccount c;
 	int i,j;
 	cout<<"Class and Encapsulation Completed"<<endl;
 	cout<<"Do you want to 1) Deposit, 2) Withdraw, 3) Check Balance"<<endl;
@@ -59,7 +91,7 @@ int main()
 		case 2:
 			cout<<"Enter Amount: "<<endl;
 			cin>>j;
-			a.withdraw(j);
+			s.withdraw(j);
 			break;
 		case 3:
 			cout<<"Enter account no. : "<<endl;
@@ -69,7 +101,26 @@ int main()
 		default:
 			cout<<"Enter correct option no.!"<<endl;
 			break;
-	}
+	}	
+	//Abstraction
+	
+    Account* accounts[2];
+    accounts[0] = new SavingsAccount();
+    accounts[1] = new CurrentAccount();
+    //
+    int choice, amt;
+    cout << "Choose Account Type: 1) Savings 2) Current" << endl;
+    cin >> choice;
+    
+    Account* selected = accounts[choice - 1];
+    
+    selected->deposit(60000);
+    selected->withdraw(30000);
+    selected->displayBalance(123456);
+    
+    for(int i = 0; i < 2; i++)
+        delete accounts[i];
+
 
 	return 0;
 }
